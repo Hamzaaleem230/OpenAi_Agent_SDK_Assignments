3a3b5be89685e5dd2ce30036b673c1d3a0602aad
Syed Hamza Aleem <162742428+Hamzaaleem230@users.noreply.github.com>
2025-08-27 21:18:58 +0500

    guardrial_input_function.py

--- Assignment 08 Implement output guardrail functionality/guardrial_function/guardrial_input_function.py
+++ Assignment 08 Implement output guardrail functionality/guardrial_function/guardrial_input_function.py
@@ -1,12 +1,15 @@
-from agents import RunContextWrapper, GuardrailFunctionOutput, input_guardrail, Runner
+from agents import RunContextWrapper, GuardrailFunctionOutput, input_guardrail, Runner, Agent, TResponseInputItem
 from my_agent.guardrial_agents import guardrial_agent
 
 
 @input_guardrail
-async def guardrial_input_fuction(ctx:RunContextWrapper, agent, input):
+async def guardrial_input_fuction(ctx: RunContextWrapper, agent: Agent,
+input: str | list[TResponseInputItem])-> GuardrailFunctionOutput:
     response = await Runner.run(guardrial_agent, input=input, context=ctx.context)
+    print(agent.name)
     return GuardrailFunctionOutput(
         output_info=response.final_output,
         tripwire_triggered=not response.final_output.is_math_homework_query
     )
+
     
