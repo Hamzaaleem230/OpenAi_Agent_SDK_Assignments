6f8ea97065ec1ffc67b3c2f7830df847099d820a
Syed Hamza Aleem <162742428+Hamzaaleem230@users.noreply.github.com>
2025-08-27 21:20:02 +0500

    my_data_schema.py

--- Assignment 08 Implement output guardrail functionality/data_schema/math_homework_output.py
+++ Assignment 08 Implement output guardrail functionality/data_schema/math_homework_output.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 
 class MathHomeworkOutput(BaseModel):
     is_math_homework_query: bool
-    is_avoid_political_topics_and_reference_query: bool
     reasoning: str
     
+class AvoidPoliticalTopicsAndReferenceOutput(BaseModel):
+    is_avoid_political_topics_and_reference_query: bool
+    reasoning: str
